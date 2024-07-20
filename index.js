b1c6f13ca3f7e414cced348eea6adf5c19491b99
kacha930 <robiwere20@gmail.com>
2024-07-20 16:15:16 +0300

    success

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+const employee = {
+    name: "Harry",
+    streetAddress: "20202 hotspur London",
+  };
+  
+  const updateEmployeeWithKeyAndValue = (employee, key, value) => {
+    return {
+      ...employee,
+      [key]: value,
+    };
+  };
+  
+  const destructivelyUpdateEmployeeWithKeyAndValue = (employee, key, value) => {
+    employee[key] = value;
+    return employee;
+  };
+  
+  const deleteFromEmployeeByKey = (employee, key) => {
+    const updatedEmployeeDetails = {
+      ...employee,
+    };
+    delete updatedEmployeeDetails[key];
+    return updatedEmployeeDetails;
+  };
+  
+  const destructivelyDeleteFromEmployeeByKey = (employee, key) => {
+    delete employee[key];
+    return employee;
+  };
